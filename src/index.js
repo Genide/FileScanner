183d1839a627ab61e7e0c9137a4505bbc007428e
Daniel Huan Nguyen <danielhuannguyen@gmail.com>
2016-03-05 20:48:10 -0600

    removed request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 var _ = require('lodash');
 var fs = require('fs');
-var request = require('request');
+var http = require('http');
 
 var scanFile = function (filepath) {
-	return filepath;
+	
 };
 
 exports.scanFile = scanFile;
