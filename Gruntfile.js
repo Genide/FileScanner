8b688e2efdf1f800d44a253a47948da44f7e1f21
Daniel Huan Nguyen <danielhuannguyen@gmail.com>
2016-03-05 18:32:05 -0600

    added watching capabilities

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function (grunt) {
+	var src = 'src/**/*.js';
+	var test = 'test/**/*.js';
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		exec: {
+			test: 'istanbul cover ./node_modules/mocha/bin/_mocha -- -c'
+		},
+		jshint: {
+			all: ['Gruntfile.js', src, test],
+			src: src,
+			test: test,
+			Gruntfile: 'Gruntfile.js'
+		},
+		watch: {
+			options: {
+				livereload: true
+			},
+			src: {
+				files: [src],
+				tasks: ['jshint:src', 'exec:test']
+			},
+			test: {
+				files: [test],
+				tasks: ['jshint:test', 'exec:test']
+			},
+			Gruntfile: {
+				files: ['Gruntfile.js'],
+				tasks: ['jshint:Gruntfile']
+			}
+		},
+		connect: {
+			server:{
+				options: {
+					livereload: true,
+					hostname: '127.0.0.1',
+					port: '9001',
+					open: true,
+					base: './coverage/lcov-report/'
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+	grunt.registerTask('default', ['exec:test', 'connect', 'watch']);
+};
