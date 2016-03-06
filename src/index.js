a127cec126d0f6365eeabddbb341f7b4a3aa5d62
Daniel Huan Nguyen <danielhuannguyen@gmail.com>
2016-03-05 22:23:44 -0600

    first version of getting file scan reports from VirusTotal

--- src/index.js
+++ src/index.js
@@ -2,8 +2,50 @@ var _ = require('lodash');
 var fs = require('fs');
 var request = require('request');
 
-var scanFile = function (filepath) {
-	
+var fileScanReportURL = 'https://www.virustotal.com/vtapi/v2/file/report';
+
+//MAKE THIS PROJECT INTO A VIRUSTOTAL OBJECT!!!!!
+
+var resultRequest = (options, callback) => {
+	//SEE IF THIS responseHandler CAN BE MADE IMMUTABLE AT SOME POINT
+	var responseHandler = (res) => {
+		if (res.statusCode === 200) {
+			var data = '';
+			res.on('data', (chunk) => {
+				data += chunk;
+			}).on('end', () => {
+				callback(JSON.parse(data));
+			});
+		} else {
+			return;
+		}
+	};
+
+	request
+		.post(options)
+		.on('response', responseHandler)
+		.on('error', () => {
+			return;
+		});
 };
 
-exports.scanFile = scanFile;
+var getFileScanReport = (resourceID) => {
+	var param = {
+		resource: resourceID,
+		apikey: ''
+	};
+	var options = {
+		url: 'https://www.virustotal.com/vtapi/v2/file/report',
+		form: param
+	};
+
+	var printToScreen = (data) => {
+		console.log(data);
+	};
+
+	resultRequest(options, printToScreen);
+}
+
+getFileScanReport('99017f6eebbac24f351415dd410d522d');
+
+//exports.scanFile = scanFile;
